bfce17ee75c97490a93e31096021dd9bd9a6e03a
Gael González <gaeldev032@gmail.com>
2024-05-11 16:05:21 -0700

    Adding router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+import { Routes, Route } from "react-router-dom";
+import * as ROUTES from "./routes/routes";
+import { Index } from "./routes/Index";
 const App = () => {
-  return <></>;
+  return (
+    <div id="app">
+      <Routes>
+        <Route exact path={ROUTES.INDEX} Component={Index} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
