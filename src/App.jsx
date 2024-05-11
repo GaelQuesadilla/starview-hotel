47e4edfdb72f54625ef8097a7178d9a512d4577d
Gael González <gaeldev032@gmail.com>
2024-05-11 16:41:47 -0700

    Adding bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import { Routes, Route } from "react-router-dom";
 import * as ROUTES from "./routes/routes";
 import { Index } from "./routes/Index";
+import { RegularTemplate } from "./layouts/RegularTemplate";
+import "bootstrap/dist/js/bootstrap";
+import "./styles/index.scss";
+
 const App = () => {
   return (
     <div id="app">
       <Routes>
-        <Route exact path={ROUTES.INDEX} Component={Index} />
+        <Route
+          exact
+          path={ROUTES.INDEX}
+          Component={() => (
+            <RegularTemplate>
+              <Index />
+            </RegularTemplate>
+          )}
+        />
       </Routes>
     </div>
   );
